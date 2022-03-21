029d2e3c3dbe42c0d92d1400da70bbaecdd99016
Piotr <straszak.piotr@gmail.com>
2022-03-21 14:55:17 +0100

    Section 13: Uploading Files

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBH6uWxw3T5vdJBi8Q4JGBGhX4PkK0GFUU',
@@ -14,11 +15,15 @@ firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth()
 const db = firebase.firestore()
+const storage = firebase.storage()
 
 const usersCollection = db.collection('users')
+const songsCollection = db.collection('songs')
 
 export {
   auth,
   db,
-  usersCollection
+  storage,
+  usersCollection,
+  songsCollection
 }
